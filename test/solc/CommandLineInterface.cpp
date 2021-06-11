a47a46d6ee7a5c06903e0aa8685edcaa87e741d4
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-06-11 16:39:28 +0200

    CLI tests for FileReader

--- test/solc/CommandLineInterface.cpp
+++ test/solc/CommandLineInterface.cpp
@@ -74,6 +74,22 @@ OptionsReaderAndMessages parseCommandLineAndReadInputFiles(vector<string> const&
 	return {success, cli.options(), cli.fileReader(), cli.standardJsonInput(), sout.str(), serr.str()};
 }
 
+CommandLineOptions defaultCommandLineOptions()
+{
+	CommandLineOptions options;
+
+	options.expectedExecutionsPerDeployment = 200;
+	options.initializeModelChecker = true;
+	options.modelCheckerSettings = {
+		ModelCheckerContracts::Default(),
+		ModelCheckerEngine::None(),
+		ModelCheckerTargets::Default(),
+		nullopt,
+	};
+
+	return options;
+}
+
 void createEmptyFilesWithParentDirs(set<boost::filesystem::path> const& _paths)
 {
 	for (boost::filesystem::path const& path: _paths)
@@ -295,6 +311,369 @@ BOOST_AUTO_TEST_CASE(standard_json_remapping)
 	OptionsReaderAndMessages result = parseCommandLineAndReadInputFiles(commandLine);
 	BOOST_TEST(!result.success);
 	BOOST_TEST(result.stderrContent == expectedMessage);
+}
+
+BOOST_AUTO_TEST_CASE(cli_paths_to_source_unit_names_no_base_path)
+{
+	TemporaryDirectory tempDirCurrent("file-reader-test-");
+	TemporaryDirectory tempDirOther("file-reader-test-");
+	TemporaryWorkingDirectory tempWorkDir(tempDirCurrent.path());
+	soltestAssert(tempDirCurrent.path().is_absolute(), "");
+	soltestAssert(tempDirOther.path().is_absolute(), "");
+
+	vector<string> commandLine = {
+		"solc",
+		"contract1.sol",                                    // Relative path
+		"c/d/contract2.sol",                                // Relative path with subdirectories
+		tempDirCurrent.path().string() + "/contract3.sol",  // Absolute path inside working dir
+		tempDirOther.path().string() + "/contract4.sol",    // Absolute path outside of working dir
+	};
+
+	CommandLineOptions expectedOptions = defaultCommandLineOptions();
+	expectedOptions.sourceFilePaths = {
+		"contract1.sol",
+		"c/d/contract2.sol",
+		tempDirCurrent.path() / "contract3.sol",
+		tempDirOther.path() / "contract4.sol",
+	};
+
+	map<string, string> expectedSources = {
+		{"contract1.sol", ""},
+		{"c/d/contract2.sol", ""},
+		{tempDirCurrent.path().string() + "/contract3.sol", ""},
+		{tempDirOther.path().string() + "/contract4.sol", ""},
+	};
+
+	FileReader::FileSystemPathSet expectedAllowedDirectories = {
+		tempDirCurrent.path() / "c/d",
+		tempDirCurrent.path(),
+		tempDirOther.path(),
+	};
+
+	createEmptyFilesWithParentDirs(expectedOptions.sourceFilePaths);
+	OptionsReaderAndMessages result = parseCommandLineAndReadInputFiles(commandLine);
+
+	BOOST_TEST(result.stderrContent == "");
+	BOOST_TEST(result.stdoutContent == "");
+	BOOST_REQUIRE(result.success);
+	BOOST_TEST((result.options == expectedOptions));
+	BOOST_TEST(result.reader.sourceCodes() == expectedSources);
+	BOOST_TEST(result.reader.allowedDirectories() == expectedAllowedDirectories);
+	BOOST_TEST(result.reader.basePath() == expectedOptions.basePath);
+}
+
+BOOST_AUTO_TEST_CASE(cli_paths_to_source_unit_names_base_path_same_as_work_dir)
+{
+	TemporaryDirectory tempDirCurrent("file-reader-test-");
+	TemporaryDirectory tempDirOther("file-reader-test-");
+	TemporaryWorkingDirectory tempWorkDir(tempDirCurrent.path());
+	soltestAssert(tempDirCurrent.path().is_absolute(), "");
+	soltestAssert(tempDirOther.path().is_absolute(), "");
+
+	vector<string> commandLine = {
+		"solc",
+		"--base-path=" + tempDirCurrent.path().string(),
+		"contract1.sol",                                    // Relative path
+		"c/d/contract2.sol",                                // Relative path with subdirectories
+		tempDirCurrent.path().string() + "/contract3.sol",  // Absolute path inside working dir
+		tempDirOther.path().string() + "/contract4.sol",    // Absolute path outside of working dir
+	};
+
+	CommandLineOptions expectedOptions = defaultCommandLineOptions();
+	expectedOptions.sourceFilePaths = {
+		"contract1.sol",
+		"c/d/contract2.sol",
+		tempDirCurrent.path() / "contract3.sol",
+		tempDirOther.path() / "contract4.sol",
+	};
+	expectedOptions.basePath = tempDirCurrent.path();
+
+	map<string, string> expectedSources = {
+		{"contract1.sol", ""},
+		{"c/d/contract2.sol", ""},
+		{tempDirCurrent.path().string() + "/contract3.sol", ""},
+		{tempDirOther.path().string() + "/contract4.sol", ""},
+	};
+
+	FileReader::FileSystemPathSet expectedAllowedDirectories = {
+		tempDirCurrent.path() / "c/d",
+		tempDirCurrent.path(),
+		tempDirOther.path(),
+	};
+
+	createEmptyFilesWithParentDirs(expectedOptions.sourceFilePaths);
+	OptionsReaderAndMessages result = parseCommandLineAndReadInputFiles(commandLine);
+
+	BOOST_TEST(result.stderrContent == "");
+	BOOST_TEST(result.stdoutContent == "");
+	BOOST_REQUIRE(result.success);
+	BOOST_TEST((result.options == expectedOptions));
+	BOOST_TEST(result.reader.sourceCodes() == expectedSources);
+	BOOST_TEST(result.reader.allowedDirectories() == expectedAllowedDirectories);
+	BOOST_TEST(result.reader.basePath() == expectedOptions.basePath);
+}
+
+BOOST_AUTO_TEST_CASE(cli_paths_to_source_unit_names_base_path_different_from_work_dir)
+{
+	TemporaryDirectory tempDirCurrent("file-reader-test-");
+	TemporaryDirectory tempDirOther("file-reader-test-");
+	TemporaryDirectory tempDirBase("file-reader-test-");
+	TemporaryWorkingDirectory tempWorkDir(tempDirCurrent.path());
+	soltestAssert(tempDirCurrent.path().is_absolute(), "");
+	soltestAssert(tempDirOther.path().is_absolute(), "");
+	soltestAssert(tempDirBase.path().is_absolute(), "");
+
+	vector<string> commandLine = {
+		"solc",
+		"--base-path=" + tempDirBase.path().string(),
+		"contract1.sol",                                    // Relative path
+		"c/d/contract2.sol",                                // Relative path with subdirectories
+		tempDirCurrent.path().string() + "/contract3.sol",  // Absolute path inside working dir
+		tempDirOther.path().string() + "/contract4.sol",    // Absolute path outside of working dir
+		tempDirBase.path().string() + "/contract5.sol",     // Absolute path inside base path
+	};
+
+	CommandLineOptions expectedOptions = defaultCommandLineOptions();
+	expectedOptions.sourceFilePaths = {
+		"contract1.sol",
+		"c/d/contract2.sol",
+		tempDirCurrent.path() / "contract3.sol",
+		tempDirOther.path() / "contract4.sol",
+		tempDirBase.path() / "contract5.sol",
+	};
+	expectedOptions.basePath = tempDirBase.path();
+
+	map<string, string> expectedSources = {
+		{"contract1.sol", ""},
+		{"c/d/contract2.sol", ""},
+		{tempDirCurrent.path().string() + "/contract3.sol", ""},
+		{tempDirOther.path().string() + "/contract4.sol", ""},
+		{tempDirBase.path().string() + "/contract5.sol", ""},
+	};
+
+	FileReader::FileSystemPathSet expectedAllowedDirectories = {
+		tempDirCurrent.path() / "c/d",
+		tempDirCurrent.path(),
+		tempDirOther.path(),
+		tempDirBase.path(),
+	};
+
+	createEmptyFilesWithParentDirs(expectedOptions.sourceFilePaths);
+	OptionsReaderAndMessages result = parseCommandLineAndReadInputFiles(commandLine);
+
+	BOOST_TEST(result.stderrContent == "");
+	BOOST_TEST(result.stdoutContent == "");
+	BOOST_REQUIRE(result.success);
+	BOOST_TEST((result.options == expectedOptions));
+	BOOST_TEST(result.reader.sourceCodes() == expectedSources);
+	BOOST_TEST(result.reader.allowedDirectories() == expectedAllowedDirectories);
+	BOOST_TEST(result.reader.basePath() == expectedOptions.basePath);
+}
+
+BOOST_AUTO_TEST_CASE(cli_paths_to_source_unit_names_relative_base_path)
+{
+	TemporaryDirectory tempDirCurrent("file-reader-test-");
+	TemporaryDirectory tempDirOther("file-reader-test-");
+	TemporaryWorkingDirectory tempWorkDir(tempDirCurrent.path());
+	soltestAssert(tempDirCurrent.path().is_absolute(), "");
+	soltestAssert(tempDirOther.path().is_absolute(), "");
+
+	vector<string> commandLine = {
+		"solc",
+		"--base-path=base",
+		"contract1.sol",                                        // Relative path outside of base path
+		"base/contract2.sol",                                   // Relative path inside base path
+		tempDirCurrent.path().string() + "/contract3.sol",      // Absolute path inside working dir
+		tempDirCurrent.path().string() + "/base/contract4.sol", // Absolute path inside base path
+		tempDirOther.path().string() + "/contract5.sol",        // Absolute path outside of working dir
+		tempDirOther.path().string() + "/base/contract6.sol",   // Absolute path outside of working dir
+	};
+
+	CommandLineOptions expectedOptions = defaultCommandLineOptions();
+	expectedOptions.sourceFilePaths = {
+		"contract1.sol",
+		"base/contract2.sol",
+		tempDirCurrent.path() / "contract3.sol",
+		tempDirCurrent.path() / "base/contract4.sol",
+		tempDirOther.path() / "contract5.sol",
+		tempDirOther.path() / "base/contract6.sol",
+	};
+	expectedOptions.basePath = "base";
+
+	map<string, string> expectedSources = {
+		{"contract1.sol", ""},
+		{"base/contract2.sol", ""},
+		{tempDirCurrent.path().string() + "/contract3.sol", ""},
+		{tempDirCurrent.path().string() + "/base/contract4.sol", ""},
+		{tempDirOther.path().string() + "/contract5.sol", ""},
+		{tempDirOther.path().string() + "/base/contract6.sol", ""},
+	};
+
+	FileReader::FileSystemPathSet expectedAllowedDirectories = {
+		tempDirCurrent.path() / "base",
+		tempDirCurrent.path(),
+		tempDirOther.path(),
+		tempDirOther.path() / "base",
+	};
+
+	createEmptyFilesWithParentDirs(expectedOptions.sourceFilePaths);
+	OptionsReaderAndMessages result = parseCommandLineAndReadInputFiles(commandLine);
+
+	BOOST_TEST(result.stderrContent == "");
+	BOOST_TEST(result.stdoutContent == "");
+	BOOST_REQUIRE(result.success);
+	BOOST_TEST((result.options == expectedOptions));
+	BOOST_TEST(result.reader.sourceCodes() == expectedSources);
+	BOOST_TEST(result.reader.allowedDirectories() == expectedAllowedDirectories);
+	BOOST_TEST(result.reader.basePath() == expectedOptions.basePath);
+}
+
+BOOST_AUTO_TEST_CASE(cli_paths_to_source_unit_names_normalization_and_weird_names)
+{
+	TemporaryDirectory tempDir("file-reader-test-");
+	create_directories(tempDir.path() / "x/y/z");
+	TemporaryWorkingDirectory tempWorkDir(tempDir.path() / "x/y/z");
+	soltestAssert(tempDir.path().is_absolute(), "");
+
+	string doubleSlashRootDir = "/" + tempDir.path().generic_string();
+	soltestAssert(doubleSlashRootDir[0] == '/' && doubleSlashRootDir[1] == '/', "");
+	soltestAssert(doubleSlashRootDir[2] != '/', "");
+
+	vector<string> commandLine = {
+		"solc",
+
+		// URLs. We interpret them as local paths.
+		"file://c/d/contract1.sol",
+		"file:///c/d/contract2.sol",
+		"https://example.com/contract3.sol",
+
+		// Redundant slashes
+		"a/b//contract4.sol",
+		"a/b///contract5.sol",
+		"a/b////contract6.sol",
+
+		// Dot segments
+		"./a/b/contract7.sol",
+		"././a/b/contract8.sol",
+		"a/./b/contract9.sol",
+		"a/././b/contract10.sol",
+
+		// Dot dot segments
+		"../a/b/contract11.sol",
+		"../../a/b/contract12.sol",
+		"a/../b/contract13.sol",
+		"a/b/../../contract14.sol",
+		tempDir.path().string() + "/x/y/z/a/../b/contract15.sol",
+		tempDir.path().string() + "/x/y/z/a/b/../../contract16.sol",
+
+		// Dot dot segments going beyond filesystem root
+		"/.." + tempDir.path().generic_string() + "/contract17.sol",
+		"/../.." + tempDir.path().generic_string() + "/contract18.sol",
+
+		// Path with two slashes (often treated specially)
+		doubleSlashRootDir + "/contract19.sol",
+
+		// Name conflict with source unit name of stdin
+		"<stdin>",
+
+#if !defined(_WIN32)
+		// Windows paths on non-Windows systems.
+		// Note that on Windows we tested them already just by using absolute paths.
+		"a\\b\\contract20.sol",
+		"C:\\a\\b\\contract21.sol",
+#endif
+	};
+
+	CommandLineOptions expectedOptions = defaultCommandLineOptions();
+	expectedOptions.sourceFilePaths = {
+		"file://c/d/contract1.sol",
+		"file:///c/d/contract2.sol",
+		"https://example.com/contract3.sol",
+
+		"a/b//contract4.sol",
+		"a/b///contract5.sol",
+		"a/b////contract6.sol",
+
+		"./a/b/contract7.sol",
+		"././a/b/contract8.sol",
+		"a/./b/contract9.sol",
+		"a/././b/contract10.sol",
+
+		"../a/b/contract11.sol",
+		"../../a/b/contract12.sol",
+		"a/../b/contract13.sol",
+		"a/b/../../contract14.sol",
+		tempDir.path().string() + "/x/y/z/a/../b/contract15.sol",
+		tempDir.path().string() + "/x/y/z/a/b/../../contract16.sol",
+
+		"/.." + tempDir.path().string() + "/contract17.sol",
+		"/../.." + tempDir.path().string() + "/contract18.sol",
+
+		doubleSlashRootDir + "/contract19.sol",
+		"<stdin>",
+
+#if !defined(_WIN32)
+		"a\\b\\contract20.sol",
+		"C:\\a\\b\\contract21.sol",
+#endif
+	};
+
+	map<string, string> expectedSources = {
+		{"file://c/d/contract1.sol", ""},
+		{"file:///c/d/contract2.sol", ""},
+		{"https://example.com/contract3.sol", ""},
+
+		{"a/b//contract4.sol", ""},
+		{"a/b///contract5.sol", ""},
+		{"a/b////contract6.sol", ""},
+
+		{"./a/b/contract7.sol", ""},
+		{"././a/b/contract8.sol", ""},
+		{"a/./b/contract9.sol", ""},
+		{"a/././b/contract10.sol", ""},
+
+		{"../a/b/contract11.sol", ""},
+		{"../../a/b/contract12.sol", ""},
+		{"a/../b/contract13.sol", ""},
+		{"a/b/../../contract14.sol", ""},
+		{tempDir.path().string() + "/x/y/z/a/../b/contract15.sol", ""},
+		{tempDir.path().string() + "/x/y/z/a/b/../../contract16.sol", ""},
+
+		{"/.." + tempDir.path().string() + "/contract17.sol", ""},
+		{"/../.." + tempDir.path().string() + "/contract18.sol", ""},
+
+		{doubleSlashRootDir + "/contract19.sol", ""},
+		{"<stdin>", ""},
+
+#if !defined(_WIN32)
+		{"a\\b\\contract20.sol", ""},
+		{"C:\\a\\b\\contract21.sol", ""},
+#endif
+	};
+
+	FileReader::FileSystemPathSet expectedAllowedDirectories = {
+		tempDir.path() / "x/y/z/file:/c/d",
+		tempDir.path() / "x/y/z/https:/example.com",
+		tempDir.path() / "x/y/z/a/b",
+		tempDir.path() / "x/y/z",
+		tempDir.path() / "x/y/z/b",
+		tempDir.path() / "x/y/a/b",
+		tempDir.path() / "x/a/b",
+		doubleSlashRootDir,
+		tempDir.path(),
+	};
+
+	createEmptyFilesWithParentDirs(expectedOptions.sourceFilePaths);
+
+	OptionsReaderAndMessages result = parseCommandLineAndReadInputFiles(commandLine);
+
+	BOOST_TEST(result.stderrContent == "");
+	BOOST_TEST(result.stdoutContent == "");
+	BOOST_REQUIRE(result.success);
+	BOOST_TEST((result.options == expectedOptions));
+	BOOST_TEST(result.reader.sourceCodes() == expectedSources);
+	BOOST_TEST(result.reader.allowedDirectories() == expectedAllowedDirectories);
+	BOOST_TEST(result.reader.basePath() == expectedOptions.basePath);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
